d05b5e8dae6e7862b8226a21ee204b7416feaf7c
Dima-PC <kikkyow18@gmail.com>
2021-12-12 22:44:13 +0400

    feat: add backlog task drag and drop

--- src/js/task.js
+++ src/js/task.js
@@ -1,5 +1,6 @@
 class Task {
-  constructor(task) {
+  constructor(task, remove) {
+    this.remove = remove;
     this.task = task;
   }
 
@@ -8,11 +9,19 @@ class Task {
 
     const listItem = document.createElement("li");
     listItem.className = "backlog-item";
+    listItem.id = this.task.id;
+    listItem.setAttribute("draggable", true);
+
+    listItem.addEventListener("dragstart", this.dragStart);
+    listItem.addEventListener("dragend", this.dragEnd);
 
     const titleElement = document.createElement("p");
     titleElement.className = "backlog-item-title";
     titleElement.innerText = subject;
 
+    listItem.append(titleElement);
+
+    //append description if it exists
     if (description) {
       const descriptionElement = document.createElement("p");
       subjectdescriptionElementText.className = "backlog-item-description";
@@ -21,9 +30,23 @@ class Task {
       listItem.append(descriptionElement);
     }
 
-    listItem.append(subjectText);
+    this.element = listItem;
 
     return listItem;
+  };
+
+  dragStart = (e) => {
+    setTimeout(() => {
+      this.element.classList.add("invisible");
+    }, 0);
+
+    const dragData = JSON.stringify(this.task);
+    e.dataTransfer.setData("text/plain", dragData);
+    e.dataTransfer.dropEffect = "data";
+  };
+
+  dragEnd = () => {
+    this.element.classList.remove("invisible");
   };
 }
 
--- src/js/user.js
+++ src/js/user.js
@@ -1,4 +1,4 @@
-import getWeekDates from "./utils/getWeekDates";
+import moment from "moment";
 
 class User {
   constructor(data) {
@@ -10,11 +10,22 @@ class User {
   }
 
   createRow = (currentWeek) => {
+    //save current week
+    this.currentWeek = currentWeek;
+
+    //container
     const rowContainer = document.createElement("div");
     rowContainer.className = "row";
 
+    //name
     const nameCell = document.createElement("div");
     nameCell.className = "cell name-cell";
+
+    //add drag events
+    nameCell.addEventListener("dragover", this.onDragOverCell);
+    nameCell.addEventListener("dragenter", this.onDragEnterCell);
+    nameCell.addEventListener("dragleave", this.onDragLeaveCell);
+    nameCell.addEventListener("drop", this.onDragDrop);
 
     const nameText = document.createElement("p");
     nameText.className = "cell-text";
@@ -23,9 +34,11 @@ class User {
     nameCell.append(nameText);
     rowContainer.append(nameCell);
 
+    //planer days
     for (let day = 0; day < 7; day++) {
       const dayCell = document.createElement("div");
       dayCell.className = "cell day-cell";
+      dayCell.dataset.date = currentWeek[day].format("YYYY-MM-DD");
 
       const dayTask = this.tasks.find((task) => {
         return (
@@ -35,18 +48,76 @@ class User {
       });
       if (dayTask) {
         dayCell.classList.add("active");
-
         const cellText = document.createElement("p");
         cellText.className = "cell-text";
         cellText.innerText = dayTask.subject;
 
         dayCell.append(cellText);
+      } else {
+        dayCell.addEventListener("dragover", this.onDragOverCell);
+        dayCell.addEventListener("dragenter", this.onDragEnterCell);
+        dayCell.addEventListener("dragleave", this.onDragLeaveCell);
+        dayCell.addEventListener("drop", this.onDragDrop);
       }
 
       rowContainer.append(dayCell);
     }
 
     return rowContainer;
+  };
+
+  onDragOverCell = (e) => {
+    e.preventDefault();
+  };
+  onDragEnterCell = (e) => {
+    e.preventDefault();
+
+    //apply hovered styles if cell is not name cell
+    if (e.target.dataset.date) {
+      e.target.classList.add("hovered");
+    }
+  };
+  onDragLeaveCell = (e) => {
+    e.target.classList.remove("hovered");
+  };
+  onDragDrop = (e) => {
+    //get data
+    const transferedData = e.dataTransfer.getData("text/plain");
+    const task = JSON.parse(transferedData);
+
+    let planStart = moment(task.planStartDate);
+    let planEnd = moment(task.planEndDate);
+    const planDayDifference = planEnd.diff(planStart, "days");
+
+    //change plan date if dragged to date cell
+    if (e.target.dataset.date) {
+      planStart = moment(e.target.dataset.date, "YYYY-MM-DD");
+      planEnd = planStart.clone().add(planDayDifference, "day");
+    }
+
+    //save data
+    this.tasks.push({
+      ...task,
+      planStartDate: planStart,
+      planEndDate: planEnd,
+    });
+
+    //remove task from backlog
+    const taskElement = document.getElementById(task.id);
+    taskElement.remove();
+
+    const newRowChildren = [...this.createRow(this.currentWeek).children];
+    const currentRow = e.target.closest(".row");
+    currentRow.innerHTML = "";
+    newRowChildren.forEach((element) => {
+      currentRow.append(element);
+    });
+
+    //remove event listeners
+    e.target.removeEventListener("dragover", this.onDragOverCell);
+    e.target.removeEventListener("dragenter", this.onDragEnterCell);
+    e.target.removeEventListener("dragleave", this.onDragLeaveCell);
+    e.target.removeEventListener("drop", this.onDragDrop);
   };
 }
 
